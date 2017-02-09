cb54d7bede5b989fc114ad8811f7f69a424a5853
hmsthunderchild <9d041f11@opayq.com>
2017-02-10 03:57:27 +1100

    Slot position

--- th_weapons/lua/weapons/weapon_th_tripmine/shared.lua
+++ th_weapons/lua/weapons/weapon_th_tripmine/shared.lua
@@ -11,6 +11,8 @@ SWEP.Contact		= ""
 SWEP.Purpose		= ""
 SWEP.Instructions	= '+attack: Place tripmine.'
 SWEP.Category		= 'They Hunger'
+SWEP.Slot				= 4
+SWEP.SlotPos			= 3
 
 SWEP.ViewModelFOV	= 90
 SWEP.ViewModelFlip	= false
