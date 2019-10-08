d930e73862026677f3ed2791e2dc1525924a84a2
Marc-Antoine Lortie <marc-antoine.lortie@outlook.com>
2019-10-08 10:35:47 -0400

    Fixed addon not loading when Half-Life Deathmatch: Source is mounted.

--- th_weapons/lua/autorun/content/hl1/mount.lua
+++ th_weapons/lua/autorun/content/hl1/mount.lua
@@ -1,8 +1,10 @@
 AddCSLuaFile()
 
 function IsHL1Mounted()
-	
-	if !IsMounted( 280 ) || !IsMounted( 'hl1' ) then return false end
-
-	return true
+	--[[
+		Return true if either Half-Life: Source or 
+		Half-Life Deathmatch: Source is mounted.
+	--]]
+	return IsMounted( 280 ) or IsMounted( 'hl1' ) or 
+		IsMounted( 360 ) or IsMounted( 'hl1mp' )
 end
